b26519486a6be855e41a157c6e44c61fc76a297f
furrki-Mac <admin@Admins-Mac.local>
2019-01-29 22:09:12 +0300

    Added Min And Max Functions

--- Xox/Module/AI.swift
+++ Xox/Module/AI.swift
@@ -11,12 +11,71 @@ import Foundation
 class AI {
     
     var game: Game
-
+    
     init(game: Game) {
         self.game = game
     }
     
-    func decide() -> Int{
+    func selectables(table: [SquareType]) -> [Int] {
+        return self.game.table.enumerated()
+            .filter { $0.element == SquareType.empty }
+            .map {
+                return $0.offset
+        }
+    }
+    
+    
+    func min(table: [SquareType]) -> Double {
+        let winConditions = game.winConditions
+        var score = 1
+        var min = 50.0
+        for condition in winConditions {
+            score = 1
+            if table[condition[0]] != .player && table[condition[1]] != .player && table[condition[2]] != .player {
+                for i in 1...3 {
+                    if table[condition[i]] == .op {
+                        score /= 2
+                    }
+                }
+            }
+            if table[condition[0]] != .op && table[condition[1]] != .op && table[condition[2]] != .op {
+                for i in 1...3 {
+                    if table[condition[i]] == .player {
+                        score *= 2
+                    }
+                }
+            }
+            min = Double.minimum(Double(score), Double(min))
+        }
+        return min
+    }
+    
+    func max(table: [SquareType]) -> Double {
+        let winConditions = game.winConditions
+        var score = 1
+        var max = 0.0
+        for condition in winConditions {
+            score = 1
+            if table[condition[0]] != .player && table[condition[1]] != .player && table[condition[2]] != .player {
+                for i in 1...3 {
+                    if table[condition[i]] == .op {
+                        score *= 2
+                    }
+                }
+            }
+            if table[condition[0]] != .op && table[condition[1]] != .op && table[condition[2]] != .op {
+                for i in 1...3 {
+                    if table[condition[i]] == .player {
+                        score /= 2
+                    }
+                }
+            }
+            max = Double.maximum(Double(score), Double(max))
+        }
+        return max
+    }
+    
+    func decide() -> Int {
         if game.checkFinish() {
             return -1
         }
